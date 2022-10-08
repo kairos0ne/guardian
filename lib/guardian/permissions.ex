317c058f5cc682879b047e6b4e25259c53d10172
Kairos0ne <kairos0ne@icloud.com>
2022-10-08 23:23:26 +0100

    check if fetching env allows runtime update

--- lib/guardian/permissions.ex
+++ lib/guardian/permissions.ex
@@ -137,7 +137,7 @@ defmodule Guardian.Permissions do
 
       defdelegate max(), to: Guardian.Permissions
 
-      raw_perms = Application.fetch_env!(__MODULE__, :permissions)
+      raw_perms = @config_with_key.(:permissions)
 
       unless raw_perms do
         raise "Permissions are not defined for #{to_string(__MODULE__)}"
