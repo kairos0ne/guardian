5b200e2220ad87c0337bc7f557131da1017657da
Kairos0ne <kairos0ne@icloud.com>
2023-10-05 14:33:10 +0100

    All files

--- lib/guardian/permissions.ex
+++ lib/guardian/permissions.ex
@@ -143,7 +143,12 @@ defmodule Guardian.Permissions do
       end
 
       def get_normalized_permissions do
-        Application.get_env(unquote(Keyword.get(opts, :otp_app, System.get_env("OTP_APP_ENV_VAR_NAME"))), __MODULE__)[:permissions]
+        # This requires that the permissions are defined in the config
+        # or passed in as an option to `use Guardian`
+        # Specify runnning application with `otp_app: :my_app`
+        # Needs to be refactored to use an application env variable
+
+        Application.get_env(unquote(Keyword.get(opts, :otp_app, :dynamic)), __MODULE__)[:permissions]
         |> Guardian.Permissions.normalize_permissions()
       end
 
