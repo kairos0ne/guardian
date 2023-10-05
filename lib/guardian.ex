fa3daede80e2952acb27b350ef9c1bc7ac79f5e1
Kairos0ne <kairos0ne@icloud.com>
2023-10-05 13:22:09 +0100

    Test

--- lib/guardian.ex
+++ lib/guardian.ex
@@ -373,7 +373,7 @@ defmodule Guardian do
       def config,
         do:
           unquote(otp_app)
-          |> Application.get_env(__MODULE__, [])
+          |> Application.fetch_env!(__MODULE__, [])
           |> Keyword.merge(unquote(opts))
 
       @doc """
